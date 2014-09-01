8b5a4c6599bf767c55168d1a1062ccccaa69a08b
Alex Meckes <alex.meckes@gmail.com>
2014-09-01 17:30:59 -0400

    Initial Commit

---
+++ test1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test1
+//
+//  Created by Alex Meckes on 9/1/14.
+//  Copyright (c) 2014 Alex Meckes. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
