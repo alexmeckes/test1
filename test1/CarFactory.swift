6404589a02c26d7ed3744cdef0e257c49c52c550
Alex Meckes <alex.meckes@gmail.com>
2014-09-01 18:29:38 -0400

    test1

---
+++ test1/CarFactory.swift
@@ -0,0 +1,16 @@
+//
+//  CarFactory.swift
+//  test1
+//
+//  Created by Alex Meckes on 9/1/14.
+//  Copyright (c) 2014 Alex Meckes. All rights reserved.
+//
+
+import Foundation
+
+class CarFactory {
+    var name = ""
+    var color = ""
+    var horsepower = 0
+    var automaticOption = false
+}
--- test1/main.swift
+++ test1/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-println("Hello, World!")
+var myFirstCar = CarFactory()
 
+    myFirstCar.name = "Mustang"
+    myFirstCar.color = "Red"
+    myFirstCar.horsepower = 200
+    myFirstCar.automaticOption = false
+    
+    println("My \(myFirstCar.name) is \(myFirstCar.color) and has \(myFirstCar.horsepower) horsepower")
+
+var mySecondCar = CarFactory()
+
+mySecondCar.name = "BMW"
+mySecondCar.color = "blue"
+mySecondCar.horsepower = 300
+mySecondCar.automaticOption = false
+
+println("My \(mySecondCar.name) is \(mySecondCar.color) and has \(mySecondCar.horsepower) horsepower")
